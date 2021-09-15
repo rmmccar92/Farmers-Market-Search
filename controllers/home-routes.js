1d977f12e47a0222c1acb8345a5304bdcd869bc5
Ryan McCarthy <rmmcc92@aol.com>
2021-09-15 12:24:22 -0400

    started the home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,4 +2,54 @@ const router = require("express").Router();
 const { User, Market, Item } = require("../models");
 // Middleware goes here
 
+// All markets
+router.get("/", async (req, res) => {
+  try {
+    const marketData = await Market.findAll({
+      // Included models/attributes will go here
+    });
+    const markets = marketData.map((market) => market.get({ plain: true }));
+    res.render("homepage", {
+      markets,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Single Markets
+
+router.get("/", async (req, res) => {
+  try {
+    const marketData = await Market.findByPk(req.params.id, {
+      //   Included models/attributes will go here
+    });
+    if (marketData) {
+      const market = marketData.get({ plain: true });
+      res.render("market", { market });
+    } else {
+      res.status(404).json({ message: "Market not found." }).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Account login and signup
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
 module.exports = router;
