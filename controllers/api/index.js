ce3df7f19da00a40901311e6e8bc509313f9c740
Ryan McCarthy <rmmcc92@aol.com>
2021-09-15 20:04:55 -0400

    forgot to save

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
 // const marketRoutes = require('./marketRoutes');
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
 // router.use('/markets', marketRoutes);
 
 module.exports = router;
-=======
-const router = require("express").Router();
-const userRoutes = require("./user-routes");
-const marketRoutes = require("./market-routes");
-
-router.use("/users", userRoutes);
-router.use("/markets", marketRoutes);
-
-module.exports = router;
->>>>>>> main
--- models/index.js
+++ models/index.js
@@ -1,11 +1,4 @@
 const User = require("./User");
-<<<<<<< HEAD
 const User = require("./Market");
 
 module.exports = { User, Market };
-=======
-const Market = require("./Market");
-const Item = require("./Item");
-
-module.exports = { User, Market, Item };
->>>>>>> main
