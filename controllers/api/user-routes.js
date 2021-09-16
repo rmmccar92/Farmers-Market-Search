d01bc6ed291937f516bc4f3a2a1661c71c459248
Ryan McCarthy <rmmcc92@aol.com>
2021-09-16 13:15:26 -0400

    functioning login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,7 +18,9 @@ router.post("/", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
 
     if (!userData) {
       res
@@ -38,6 +40,7 @@ router.post("/login", async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.username = userData.username;
       req.session.logged_in = true;
 
       res.json({ user: userData, message: "You are now logged in!" });
