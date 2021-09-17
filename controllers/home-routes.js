be9cb3163bc3c25482b1ace35ef184a56df6f068
Ryan McCarthy <rmmcc92@aol.com>
2021-09-17 15:33:38 -0400

    logout route works

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,7 +23,7 @@ router.get("/", withAuth, async (req, res) => {
 
 // Account login and signup
 router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/");
     return;
   }
@@ -32,7 +32,7 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/");
     return;
   }
