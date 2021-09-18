42d7ebf366b0cd68cf50239dca2f1671104e07f2
Casey Twine <casey7twine@gmail.com>
2021-09-18 13:25:41 -0400

    updated user signup

--- public/JS/signup.js
+++ public/JS/signup.js
@@ -3,11 +3,17 @@ const signupFormHandler = async function(event) {
 
   const usernameEl = document.querySelector('#username-input-signup');
   const passwordEl = document.querySelector('#password-input-signup');
+  const firstNameEl = document.querySelector('#firstName-input-signup');
+  const lastNameEl = document.querySelector('#lastName-input-signup');
+  const emailEl = document.querySelector('#email-input-signup');
 
   const response = await fetch('/api/user', {
     method: 'POST',
     body: JSON.stringify({
+      first_name: firstNameEl.value,
+      last_name: lastNameEl.value,
       username: usernameEl.value,
+      email: emailEl.value,
       password: passwordEl.value,
     }),
     headers: { 'Content-Type': 'application/json' },
