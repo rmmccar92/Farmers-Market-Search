632d3bf5de6ee5ae861233891b19985d5c917d6f
Ryan McCarthy <rmmcc92@aol.com>
2021-09-20 13:44:52 -0400

    added markets to profile page and individual market pages

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Market } = require("../../models");
+
+router.get("/", async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const userData = await User.findAll({
+      include: [{ model: Market }],
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post("/", async (req, res) => {
   try {
