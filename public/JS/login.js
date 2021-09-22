a715c08284e504cab808e93e9d51b4d4490bccba
Zach Holloway <zachholloway.n@gmail.com>
2021-09-22 18:57:41 -0400

    changed landing page to dashboard on submit

--- public/JS/login.js
+++ public/JS/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async function (event) {
   });
 
   if (response.ok) {
-    document.location.replace("/home");
+    document.location.replace("/dashboard");
   } else {
     alert("Failed to login");
   }
