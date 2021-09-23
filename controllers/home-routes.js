1d75d39a72a7c5121a551937921d2e2ef70016d8
Ryan McCarthy <rmmcc92@aol.com>
2021-09-23 14:43:34 -0400

    search is now functional

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { User, Market } = require("../models");
 const withAuth = require("../utils/auth");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 router.get("/", async (req, res) => {
   res.render("homepage", {
@@ -54,16 +56,26 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/results", (req, res) => {
-  const marketData = req.session.marketData;
-  console.log(marketData);
-
-  {
-    res.render("all-markets", {
-      logged_in: req.session.logged_in,
-      markets: marketData,
-      // markets: req.session.savedMarkets,
+router.get("/search", async (req, res) => {
+  let { search } = req.query;
+  console.log(req.query);
+  try {
+    console.log({ search });
+    const searchData = await Market.findAll({
+      where: {
+        zipcode: {
+          [Op.eq]: parseInt(req.query.zipcode),
+        },
+      },
     });
+    const markets = searchData.map((market) => market.get({ plain: true }));
+    console.log(markets);
+    res.render("all-markets", {
+      markets,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
